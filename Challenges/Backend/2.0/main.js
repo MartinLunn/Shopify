a7550f09d8f99f7d3ecc88b997b9476f4116089e
MartinLunn <martin.lunn@hotmail.com>
2017-09-11 08:52:53 -0400

    inital for 3.0; node_modules.

--- Challenges/Backend/2.0/main.js
+++ Challenges/Backend/2.0/main.js
@@ -5,35 +5,49 @@
 const https = require('https');
 const fs = require('fs');
 
+
+let keepLogs = true;
+
+//should take command line arg generally speaking instead of hard-coding
 let APIEndpointURL = "https://backend-challenge-winter-2017.herokuapp.com/customers.json?page=";
 
-let APIEndpointURLIDX = 0;
+let APIEndpointURLIDX = 0;  //TODO reallyused?? maybe remove
 
-let input = undefined;
+let input = [];
 
 let output = undefined;
 
 const req = https.request( (APIEndpointURL + APIEndpointURLIDX) , (res) => {
   let now = Date.now();
-  fs.appendFile(
-    "logs.txt",
-    "Date: " + now + "\n" +
-    'Response Status Code: ' + res.statusCode + "\n" +
-    'Response Headers: ' + JSON.stringify(res.headers) + "\n" +
-    "end of log at: " + now + "\n\n",
 
-    function (err) {
-      if (err) {
-        console.log("Could not write to logfile.")
-        throw err;
-      }
-    console.log("\nLog for " + now + " Saved!\n");
-  });
+  if (keepLogs)
+  {
+    fs.appendFile(
+      "logs.txt",
+      "Date: " + now + "\n" +
+      'Response Status Code: ' + res.statusCode + "\n" +
+      'Response Headers: ' + JSON.stringify(res.headers, null, "\t") + "\n" +
+      "end of log at: " + now + "\n\n",
+
+      function (err) {
+        if (err) {
+          console.log("Could not write to logfile.")
+          throw err;
+        }
+
+      console.log("\nLog for " + now + " Saved!\n");
+    });
+  }
+
+
 
   res.on('data', (d) => {
-    input = JSON.parse(d);
     process.stdout.write(d);                                //TODO
-    console.log("\n\nThis is my input: " + input + "\n");
+    input[0] = (JSON.parse(d));
+    //console.log("\n\nThis is my input: " + JSON.stringify(input[0]) + "\n");
+
+    //callback call for calculate pagination
+    calculatePagination(input[0]);
   });
 });
 
@@ -41,3 +55,66 @@ req.on('error', (e) => {
   console.error(e);
 });
 req.end();
+
+let calculatePagination = function(page1)
+{
+  let numPages = Math.ceil(page1.pagination.total / page1.pagination.per_page);
+
+  //callback for loop
+  load(numPages);
+}
+
+let load = function(numPages)
+{
+  for (let i = 1; i < numPages; ++i)
+  {
+
+
+
+    input[i] = https.request( (APIEndpointURL + i) , (res) => {
+      let now = Date.now();
+
+      console.log("Date: " + now);
+
+      if (keepLogs)
+      {
+        fs.appendFile(
+          "logs.txt",
+          "Date: " + now + "\n" +
+          'Response Status Code: ' + res.statusCode + "\n" +
+          'Response Headers: ' + JSON.stringify(res.headers, null, "\t") + "\n" +
+          "end of log at: " + now + "\n\n",
+
+          function (err) {
+            if (err) {
+              console.log("Could not write to logfile.")
+              throw err;
+            }
+
+          console.log("\nLog for " + now + " Saved!\n");
+        });
+      }
+
+
+
+      res.on('data', (d) => {
+        process.stdout.write(d);                                //TODO
+        input[i] = (JSON.parse(d));
+
+        console.log('\nin the loop: ' + i + '\n\n\n');
+        for (let j = 0; j < input.length; ++j)
+        {
+          console.log("j: " + j + "\n");
+          console.log("input[j]: " + JSON.stringify(input[j], null, "\t") + "\n\n" );
+        }
+
+        //console.log("\n\nThis is my input: " + JSON.stringify(input[0]) + "\n");
+      });
+    });
+
+    req.on('error', (e) => {
+      console.error(e);
+    });
+    req.end();
+  }
+}
---
+++ Challenges/Backend/3.0-node-request/backendChallenge-Node.js
@@ -0,0 +1,131 @@
+/*
+Assumptions:
+1. Assuming pagination is consistent between pages with regard to total and per_page
+2. Assuming validations are consistent between pages for a given URL or set of pages
+3. No property in validations can be called __proto__ or something like that
+4. the type field in the provided validation constraints, when provided, only specifies string, number, or boolean. Behavior is undefined.
+*/
+
+let output = {invalid_customers: []};
+
+let apiEndpointURL = "https://backend-challenge-winter-2017.herokuapp.com/customers.json";
+
+let apiEndpointURLSuffix = "?page=";
+
+let request = require("request");
+
+/*//TODO make async
+//TODO explore whether we can get async validation as the data comes in
+function httpGet(theUrl)
+{
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}*/
+
+let pages = [];
+
+//getting the 0th page of input data
+pages[0] = JSON.parse(httpGet(apiEndpointURL));
+
+//figuring out how many pages there are
+let numPages = Math.ceil(pages[0].pagination.total / pages[0].pagination.per_page);
+
+//works: Math.ceil(page0.pagination.total / page0.pagination.per_page);
+//JSON.parse('{"result":1}')        don't forget the '' outside the braces
+
+let  paginatedURL = "";
+
+for (let  i = 1; i < numPages; i++)
+{
+  paginatedURL = apiEndpointURL + apiEndpointURLSuffix + (i + 1).toString();        //i + 1 because the page count starts at 1, whereas indices start at 0
+  pages[i] = JSON.parse(httpGet(paginatedURL));
+}
+
+//TODO remove
+let a = 0;
+
+function validateCustomerPage(page)
+{
+  if (!page)
+  {
+    //TODO error handle
+    console.log("page is falsy from inside validate customer page.");
+    return;
+  }
+
+  if (!(page.validations && page.customers && page.pagination))     //if page doesn't have all of these. syntax is such due to faster short-circuit evaluation
+  {
+
+    console.log("aa");
+
+
+    //TODO error handle
+    console.log("page object is missing keys or no validations listed from inside validate customer page");
+    return;
+  }
+
+  for (let i = 0; i < page.customers.length; ++i)   //main loop validating customers
+  {
+
+    for (let j = 0; j < page.validations.length; ++j)
+    {
+      let  nameOfField = Object.keys(page.validations[j])[0];  //get the field name
+
+      let  validation = page.validations[j][nameOfField];
+
+      if (validation.required && !page.customers[i].hasOwnProperty(nameOfField) )  //if field is required and not present. also quick because of short-circuit. note that this DOES NOT work for inherited properties, but is faster than using "in" which is alternate
+      {
+        //TODO
+        //console.log
+        reportInvalidCustomerFields(page.customers[i], nameOfField);
+      }
+
+      if (validation.length) //length is truthy
+      {
+        //storing length for more readable code
+        //if the customer data is null, cannot get length, therefore must check first
+        let strlen;
+        page.customers[i][nameOfField] ? strlen = page.customers[i][nameOfField].length : reportInvalidCustomerFields(page.customers[i], nameOfField);
+        if (strlen >  validation.length.max || strlen < validation.length.min) //If length is greater than max, or lesser than min. short-circuit works properly
+        {
+          //TODO
+          //console.log
+          reportInvalidCustomerFields(page.customers[i], nameOfField);
+        }
+      }
+
+      else if (validation.type)
+      {
+        if (validation.type !== typeof(page.customers[i][nameOfField]))    //else if because length assumes string meaning if length exists, it can be assumed to be a string.
+        //above statement: if the given validation type constraint matches the type of the customers data
+        {
+          //TODO
+          //console.log
+          reportInvalidCustomerFields(page.customers[i], nameOfField);
+        }
+      }
+    }
+  }
+}
+
+function reportInvalidCustomerFields(customer, field)
+{
+  if (output.invalid_customers.length > 0 && customer.id === (output.invalid_customers[output.invalid_customers.length - 1].id))
+  {
+    output.invalid_customers[output.invalid_customers.length - 1].invalid_fields.push(field);
+  }
+  else
+  {
+    output.invalid_customers.push({"id": customer.id, "invalid_fields": [field]});
+  }
+}
+
+
+for (let i = 0; i < pages.length; ++i)
+{
+  validateCustomerPage(pages[i]);
+}
+
+console.log(output);
