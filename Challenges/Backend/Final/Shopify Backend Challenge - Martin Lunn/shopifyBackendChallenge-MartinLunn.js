174c81df1540f9eaadb305b2f84479034d885f59
MartinLunn <martin.lunn@hotmail.com>
2017-09-13 18:13:16 -0400

    submitted. Fingers crossed.

--- Challenges/Backend/Final/Shopify Backend Challenge - Martin Lunn/shopifyBackendChallenge-MartinLunn.js
+++ Challenges/Backend/Final/Shopify Backend Challenge - Martin Lunn/shopifyBackendChallenge-MartinLunn.js
@@ -5,11 +5,11 @@ Assumptions:
 3. No property in validations can be called __proto__ or something like that
 4. the type field in the provided validation constraints, when provided, only specifies string, number, or boolean. Otherwise, behavior is undefined.
 5. If the validation for a field is no present, anything goes.
-
-TODO double check weirdness with null and not required fields
 6. When the field is not required, but the type is specified, null is not a valid value
 7. If the field is not required, undefined is the only acceptable non-answer.
 */
+
+//tested on node 6
 
 "use strict";
 
@@ -17,19 +17,20 @@ var request = require('request');
 
 let output = {invalid_customers: []};
 
+//could easily take this as a command line arg/parameter
 let apiEndpointURL = "https://backend-challenge-winter-2017.herokuapp.com/customers.json?page=";
 
 //pages is an array where each index represents a page of data from the api endpoint
 let pages = [];
 
-//pageCounter is a variable representing which page of the api endpoint 
+//pageCounter is a variable representing which page of the api endpoint
 let pageCounter = 1;
 
-//Because I'm using node's async features, all we need to do is call this
+//Because I'm using node's async features, all we need to do is call calculateTotalPages
+//(done at the end of the file)
 //and the rest will chain on. I could put this into a control objects
-//for improved adherence to software engineering principles, but 
+//for improved adherence to software engineering principles, but
 //those depend on context; this has no context really.
-calculateTotalPages(apiEndpointURL);
 
 //figuring out how many pages there are
 let calculateTotalPages = function(apiEndpointURL)
@@ -102,7 +103,7 @@ let validateCustomerPage = function(page)
   }
 
   //if page doesn't have all of these. syntax is such due to faster short-circuit evaluation
-  if (!(page.validations && page.customers && page.pagination))     
+  if (!(page.validations && page.customers && page.pagination))
   {
     console.error("page object is missing keys or no validations listed from inside validate customer page");
     return;
@@ -114,9 +115,9 @@ let validateCustomerPage = function(page)
     {
       let  nameOfField = Object.keys(page.validations[j])[0];  //get the field name
       let  validation = page.validations[j][nameOfField];
-      //if field is required and not present. also quick because of short-circuit. 
+      //if field is required and not present. also quick because of short-circuit.
       //note that this DOES NOT work for inherited properties, but is faster than using "in" which is alternate
-      if (validation.required && !page.customers[i].hasOwnProperty(nameOfField) )  
+      if (validation.required && !page.customers[i].hasOwnProperty(nameOfField) )
       {
         reportInvalidCustomerFields(page.customers[i], nameOfField);
       }
@@ -128,7 +129,7 @@ let validateCustomerPage = function(page)
         let strlen;
         page.customers[i][nameOfField] ? strlen = page.customers[i][nameOfField].length : reportInvalidCustomerFields(page.customers[i], nameOfField);
         //If length is greater than max, or lesser than min. short-circuit works properly
-        if (strlen >  validation.length.max || strlen < validation.length.min) 
+        if (strlen >  validation.length.max || strlen < validation.length.min)
         {
           reportInvalidCustomerFields(page.customers[i], nameOfField);
         }
@@ -158,7 +159,8 @@ let reportInvalidCustomerFields = function (customer, field)
   }
 }
 
-let printOutput = function(output)      /*console.log() wasn't playing nice
+let printOutput = function(output)
+/*console.log() wasn't playing nice
 it was outputting [Object object] if I passed in anything but exactly one object,
 and it was adding newlines at the end of every statement so,
 I decided to do my own.*/
@@ -187,8 +189,6 @@ I decided to do my own.*/
     return;
   }
 
-
-
   if (output.invalid_customers.length === 0)
   {
     console.log("There are no invalid customers.");
@@ -208,14 +208,15 @@ I decided to do my own.*/
     {
       console.log(output.invalid_customers[i]);
     }
-
     process.stdout.write("      ");
     console.log(output.invalid_customers[i]);
     //couldn't quite get this to work properly. It's missing commas.
-	  //The requirements specified an output "similar to" this. 
+	  //The requirements specified an output "similar to" this.
 	  //Based on the instructions, I believe this is acceptable.
     //process.stdout.write(",");
   }
-
   console.log("   ]\n}\n\n");
 }
+
+
+calculateTotalPages(apiEndpointURL);
